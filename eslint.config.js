3e91c9edb20f4293add623a4d058144105eb3ab9
jeffkaberagithara-guru <jeffkaberagithara@gmail.com>
2026-09-01 00:06:28 +0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,15 +2,19 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import react from 'eslint-plugin-react'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'dist-ssr']),
   {
     files: ['**/*.{js,jsx}'],
+    plugins: {
+      react,
+    },
     extends: [
       js.configs.recommended,
-      reactHooks.configs.flat.recommended,
+      reactHooks.configs.flat['recommended-latest'],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
@@ -24,6 +28,13 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react/jsx-uses-vars': 'error',
+    },
+  },
+  {
+    files: ['src/entry-server.jsx'],
+    rules: {
+      'react-refresh/only-export-components': 'off',
     },
   },
 ])
